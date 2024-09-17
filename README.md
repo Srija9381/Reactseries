8553eb95702424394e02eee1d86691cdc427b79e
Srija9381 <Srijareddyranabothu@gmail.com>
2024-09-17 10:24:19 +0530

    updates code

---
+++ README.md
@@ -0,0 +1,6 @@
+Learnt the basic of React
+what is React
+Npm & Npx
+create the app using the CDN
+what package.json & packlock.json
+dev-dependency and dependencies
---
+++ main.js
@@ -0,0 +1,20 @@
+// let heading=document.createElement("h1")
+// heading.innerHTML=("hello React Dev😎")
+
+// let root=document.getElementById("root")
+// root.appendChild(heading)
+
+
+import React from "react"
+
+let heading=React.createElement("h1",{id:"id1"},"i am react dev")
+
+   let wrap=React.createElement("div",{},  //div1
+    React.createElement("div",{},          //div2
+       [ React.createElement("h1",{},"i am senior react dev"), React.createElement("p",{},"i am also dev")]
+    )
+   )
+   
+
+   let root=ReactDOM.createRoot(document.getElementById("root"))
+   root.render(wrap)
